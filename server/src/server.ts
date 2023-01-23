823c5ed3049dcddd3f91bb4c3c7b211671920b8c
EduardTeixeira <eduardteixeira01@gmail.com>
2023-01-23 16:46:08 -0300

    aula 2 - backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,15 @@
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
 
+import { appRoutes } from './routes';
+
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors, {
     origin: ['http://localhost:3000']
 });
 
-/**
- * Métodos HTTP ::: Get, Post, Put, Patch, Delete
-*/
-app.get('/habits', async () => {
-
-    const habits = await prisma.habit.findMany({
-        /*
-        where: {
-            title: {
-                startsWith: 'Beber',
-            }
-        }
-        */
-    });
-
-    return habits;
-})
+app.register(appRoutes);
 
 app.listen({
     port: 3333,
