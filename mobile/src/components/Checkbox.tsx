09789cecc42bdac45e9be500bfd4e2d674d4158c
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-02-15 17:05:25 -0300

    aplicando animações ao app mobile

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,5 +1,6 @@
 import { Feather } from '@expo/vector-icons';
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated';
 import colors from 'tailwindcss/colors';
 
 interface Props extends TouchableOpacityProps {
@@ -14,25 +15,26 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
       className="flex-row mb-2 items-center"
       {...rest}
     >
-
       {
         checked
           ?
-          <View className='h-8 w-8 bg-green-500 rounded-lg items-center justify-center'>
+          <Animated.View
+            className='h-8 w-8 bg-green-500 rounded-lg items-center justify-center'
+            entering={ZoomIn}
+            exiting={ZoomOut}
+          >
             <Feather
               name='check'
               size={20}
               color={colors.white}
             />
-          </View>
+          </Animated.View>
           :
           <View className='h-8 w-8 bg-zinc-900 rounded-lg' />
       }
-
       <Text className='text-white text-base ml-3 font-semibold'>
         {title}
       </Text>
-
     </TouchableOpacity>
   )
 }
