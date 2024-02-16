0525c01e7f502de7da5f828172757c029292b58a
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-02-16 15:38:30 -0300

    web - notificação push

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,14 @@
 import './src/lib/dayjs';
 
 import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
-import { StatusBar, StyleSheet } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { Button, StatusBar, StyleSheet } from 'react-native';
 
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
 
 export default function App() {
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
@@ -14,17 +16,48 @@ export default function App() {
     Inter_800ExtraBold,
   });
 
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    })
+  })
+
+  async function scheduleNotification() {
+
+    const trigger = new Date(Date.now());
+
+    trigger.setSeconds(trigger.getSeconds() + 5);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Olá, Eduard! 🤩',
+        body: 'Já praticou seus hábitos hoje?'
+      },
+      trigger
+    });
+
+  }
+
+  async function getScheduleNotification() {
+    const schedules = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(schedules);
+  }
+
   if (!fontsLoaded) {
     return <Loading />;
   }
 
   return (
     <>
+      <Button title='Enviar Notificação' onPress={scheduleNotification}></Button>
+      <Button title='Verificar Notificações' onPress={getScheduleNotification}></Button>
       <Routes />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
-        translucent
+      //translucent
       />
     </>
   );
